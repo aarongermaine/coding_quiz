c18d96502b1e4518ce60e898a8142925bb4bba89
aarongermaine <vcnts0@gmail.com>
2021-01-21 18:43:56 -0700

    Javascript functionality additions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,50 @@
-function startQuiz() {
-  var x = document.getElementById("quizShow");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+const startButton = document.getElementById("start");
+const questionContainer = document.getElementById("quizShow");
+var questionEl = document.getElementById("question");
+const answerButtonEl = document.getElementById("answer-buttons");
+const nextButtonEl = document.getElementById("next");
+
+const questionList = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint",
+    },
+    correctAnswer: "d",
+  },
+];
+
+function loadQuiz() {
+  questionContainer.classList.add("show");
+  questionEl = questionList[question[0]];
+}
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  startButton.classList.add("hidden");
+  loadQuiz();
 }
 
 //GIVEN I am taking a code quiz /press start button /load quiz page
