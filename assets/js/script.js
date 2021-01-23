4b53aa9dd86d058333cee55dcaf622b9c2a12e44
aarongermaine <vcnts0@gmail.com>
2021-01-23 16:55:30 -0700

    Score and Time attribute additions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,120 @@
-const startButton = document.getElementById("start");
-const questionContainer = document.getElementById("quizShow");
-var questionEl = document.getElementById("question");
-const answerButtonEl = document.getElementById("answer-buttons");
-const nextButtonEl = document.getElementById("next");
+//const startButton = document.getElementById("start");
+//const questionContainer = document.getElementById("quizShow");
+const choices = Array.from(
+  document.getElementsByClassName("button-label")
+);
+const question = document.getElementById("question");
+var timer = document.getElementById("timeSet");
+var score = document.getElementById("scoreSet");
+//const answerButtonEl = document.getElementById("answer-buttons");
+//const nextButtonEl = document.getElementById("next");
 
-const questionList = [
+let currentQuestion = {};
+let acceptingAnswers = false;
+let questionCounter = 0;
+let availableQuestions = [];
+
+
+
+let questions = [
   {
     question: "Who invented JavaScript?",
-    answers: {
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich",
-    },
-    correctAnswer: "c",
+
+    choice1: "Douglas Crockford",
+    choice2: "Sheryl Sandberg",
+    choice3: "Brendan Eich",
+
+    answer: 3,
   },
   {
     question: "Which one of these is a JavaScript package manager?",
-    answers: {
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm",
-    },
-    correctAnswer: "c",
+
+    choice1: "Node.js",
+    choice2: "TypeScript",
+    choice3: "npm",
+
+    answer: 3,
   },
   {
     question: "Which tool can you use to ensure code quality?",
-    answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint",
-    },
-    correctAnswer: "d",
+
+    choice1: "Angular",
+    choice2: "jQuery",
+    choice3: "ESLint",
+
+    answer: 3,
   },
 ];
 
-function loadQuiz() {
-  questionContainer.classList.add("show");
-  questionEl = questionList[question[0]];
+const correctScore = 100;
+const maxQuestions = 3;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  
+  getNewQuestion();
+  startTimer();
+};
+
+setScore = () => {
+
+};
+
+startTimer = () => {
+  var seconds = 30;
+  setInterval(function() {
+    timer.innerHTML = seconds--
+  }, 1000);
+
 }
 
-startButton.addEventListener("click", startGame);
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-function startGame() {
-  startButton.classList.add("hidden");
-  loadQuiz();
-}
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    let selectedAnswer = selectedChoice.dataset["number"];
+    
+    let applyMe = 
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    
+     console.log(applyMe);
+    selectedChoice.parentElement.classList.add(applyMe);
+
+    setTimeout( () => {
+      selectedChoice.parentElement.classList.remove(applyMe);
+      getNewQuestion();
+    }, 1000);
+
+    
+
+    
+  });
+});
+
+startGame();
 
 //GIVEN I am taking a code quiz /press start button /load quiz page
 //WHEN I click the start button
